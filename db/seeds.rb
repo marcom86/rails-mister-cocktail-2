2e2562ef01269647fa6603598ac2b77da09f169e
Marco Montis <marcmontis7@gmail.com>
2020-03-24 16:05:14 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Cocktail.destroy_all if Rails.env.development?
+puts "Destroying all Cocktails"
+
+Ingredient.destroy_all if Rails.env.development?
+puts "Destroying all Ingredients"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "Create #{i.name}"
+end
